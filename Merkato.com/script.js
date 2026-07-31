93e8a48d3cc48917d5b20f6796d534ecf3ceb78b
Tegegn Wukianos <tegegnw1997@gmail.com>
2026-07-31 12:01:31 +0300

    refactor addtocart and updatecart functions

--- Merkato.com/script.js
+++ Merkato.com/script.js
@@ -4,30 +4,39 @@ const cart = {}
 function addtocart(name,image,price){
     console.log(`item add to cart ${name}`)
 
-        let mycartitems_templates = `
-        <div class="cart-item">
-                    <img src="${image}" height="100" width="100">
-                    <div class="cart-item-info">
-                        <h4>test - ${name}</h4>
-                        <p class="price">${price}birr</p>
-                    </div>
-                    <input type="number" value="1" min="1" max="10" class="qty-input">
-                    <span class="cart-total">45,000 BIRR</span>
-                    <button class="btn remove-btn">Remove</button>
-                </div>
-        `
         // cartitemdv.insertAdjacentHTML('beforeend',mycartitems_templates)
         if(cart[name]){
             console.log("item alreay exist");}
         else{
             cart[name] = {
+                    name:name,
                     quantity: 1,
-                    totalPrice: productPrice
+                    image:image,
+                    totalPrice: price
                 };
         }
         console.log(cart)
+        updatecart();
         }
 
+function updatecart(){
+for (let product in cart){
+        console.log("item in cart",cart[product],product);
+        let curnt_cart = `<div class="cart-item">
+                    <img src="${cart[product].image}" height="100" width="100">
+                    <div class="cart-item-info">
+                        <h4>test -  ${product}</h4>
+                        <p class="price">${cart[product].price}birr</p>
+                    </div>
+                    <input type="number" value="1" min="1" max="10" class="qty-input">
+                    <span class="cart-total">45,000 BIRR</span>
+                    <button class="btn remove-btn">Remove</button>
+                </div> `
+        cartitemdv.insertAdjacentHTML("beforeend",curnt_cart);
+
+
+    }
+}
     const productdiv = document.getElementById("productdiv")
     const products = [
             {name:"Hp",price:230,category:"computer",rating:4.5,imagelink:"https://images.unsplash.com/photo-1541807084-5c52b6b3adef?q=80&w=687&auto=format&fit=crop","instock":true},
@@ -52,6 +61,23 @@ function format_myproducts(name,image,price,cat,in_stock){
                 <a href="checkout.html"><button class="btn btn-green">BUY</button></a>
             </div>
         `
+
+function formatcart(name,image,price){
+    let mycartitems_templates = `
+        <div class="cart-item">
+                    <img src="${image}" height="100" width="100">
+                    <div class="cart-item-info">
+                        <h4>test - ${name}</h4>
+                        <p class="price">${price}birr</p>
+                    </div>
+                    <input type="number" value="1" min="1" max="10" class="qty-input">
+                    <span class="cart-total">45,000 BIRR</span>
+                    <button class="btn remove-btn">Remove</button>
+                </div>
+        `
+        return my_product_template
+}
+
 return my_product_template
 }
     
