8b8c9b46812aa8183b737e2e6a4c26345420a90c
Tegegn Wukianos <tegegnw1997@gmail.com>
2026-08-04 00:27:01 +0300

    add middleware  for checking logins and email conformations

---
+++ Social Media/backend/src/middleware.js
@@ -0,0 +1,31 @@
+import express from "express";
+import supabase from "./supabase.js";
+
+//check auth
+const protect = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ error: "Unauthorized" });
+    }
+    const token = authHeader.split(" ")[1];
+    const { data, error } = await supabase.auth.getUser(token);
+    if (error || !data.user) {
+        return res.status(401).json({ error: "Unauthorized" });
+    }
+    req.user = data.user;
+    next();
+};
+
+export { protect };
+
+
+// is eail verfied
+const isEmailVerified = (req, res, next) => {
+    const user = req.user;
+    if (!user.email_confirmed_at) {
+        return res.status(403).json({ error: "Email not verified" });
+    }
+    next();
+};
+
+export { isEmailVerified };
